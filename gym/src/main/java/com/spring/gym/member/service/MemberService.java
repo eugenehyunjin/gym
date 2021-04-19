c33aa2fd4fad4e208ebd9b523650e8e26e1479d4
eugen <eugen@DESKTOP-KOMKJ74>
2021-04-19 16:05:34 +0900

    커밋

--- gym/src/main/java/com/spring/gym/member/service/MemberService.java
+++ gym/src/main/java/com/spring/gym/member/service/MemberService.java
@@ -3,11 +3,6 @@ package com.spring.gym.member.service;
 import com.spring.gym.member.vo.MemberVO;
 
 public interface MemberService {
-
 	public int checklogin(MemberVO memberVO);
-
 	public int insertMember(MemberVO memberVO);
-
-
-
 }
